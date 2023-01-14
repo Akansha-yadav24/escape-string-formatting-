1d2cd6ff38c5d6c62b1bbd365d55edb2f5ce04bf
coder <108837892+Akansha-yadav24@users.noreply.github.com>
2023-01-14 11:22:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#when you want to print single backslash
+print("C:\\Python32\\Lib")
+print('Who\'s this?')
+#printing in new line
+print("This is printed\n in two lines")
+#printing hex value using excape sequence
+print("This is \x48\x45\x58 representation")
+print("\x48\x45\x4C\x4C\x4F\x20\x57\x4F\x52\x4C\x44")
+#remove spaces
+print('Interview \bBit')
+#add spaces
+print('Interview\tBit')
+#printing  alphabet using octal value
+print("\110\105\114\114\117\040\127\117\122\114\104")
+#Remove all escape sequences from a list
+s = ['Hello','\x50','to','\x44','World']
+print(s)
+#Python escape sequence ignore
+#to remove using R or r
+print(r"Hello\nWorld")
+#To remove all the characters from the left and right of an argument string, we will use the string.strip() function.
+s = '\r\r\b InterviewBit \r\r\n  '
+s.strip
+print(s)
+print("C:\Interview Bit\nScaler\aAcademy")
+print(r"C:\Users\Dell\OneDrive\Desktop")
+
